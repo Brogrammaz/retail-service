60199e87c273d6ff763b3ad88d2ec46a7294e992
njogu <dankhareem@gmail.com>
2023-01-09 16:37:11 +0300

    Added Fault Tolerance functionality to the order service

--- order-service/src/main/java/com/kharim/orderservice/controllers/OrderController.java
+++ order-service/src/main/java/com/kharim/orderservice/controllers/OrderController.java
@@ -2,10 +2,15 @@ package com.kharim.orderservice.controllers;
 
 import com.kharim.orderservice.dto.OrderRequest;
 import com.kharim.orderservice.services.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -16,8 +21,15 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<OrderRequest> placeOrder(@RequestBody OrderRequest orderRequest){
+    @CircuitBreaker(name = "inventory", fallbackMethod = "placeOrderFallback")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<ResponseEntity<OrderRequest>> placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
-        return ResponseEntity.ok(orderRequest);
+        return CompletableFuture.supplyAsync(() -> ResponseEntity.ok(orderRequest));
+    }
+
+    public CompletableFuture<String> placeOrderFallback(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(() -> "Unfortunately something went wrong please make the request later.");
     }
 }
